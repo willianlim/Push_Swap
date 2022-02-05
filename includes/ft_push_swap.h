ea7ffa040d1b03823beb674b246f03e10133399d
Willian-code <willianrosendodelima@outlook.com>
2022-02-05 11:23:12 -0300

    add ft_sort_others

--- includes/ft_push_swap.h
+++ includes/ft_push_swap.h
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 10:39:28 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/02/01 17:15:08 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/02/04 22:59:58 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@
 int		ft_push_swap(int argc, char *argv[]);
 void	ft_sort_three(t_circlist *l);
 void	ft_sort_five(t_circlist *l, t_circlist *lb);
+void	ft_sort_others(t_circlist *l, t_circlist *lb, int pivot, size_t n);
 
 #endif
