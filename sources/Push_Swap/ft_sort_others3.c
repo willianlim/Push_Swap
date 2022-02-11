f9925297b004ea38c21ea74b252084f5ff9c06c0
Willian-code <willianrosendodelima@outlook.com>
2022-02-11 18:54:00 -0300

    funciona

--- sources/Push_Swap/ft_sort_others3.c
+++ sources/Push_Swap/ft_sort_others3.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 15:48:34 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/02/11 16:37:24 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/02/11 18:53:16 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ size_t	ft_a_to_b(t_circlist *l, t_circlist *lb, size_t n, int bol)
 	i = -1;
 	while (++i <= n && l->size >= 3)
 	{
-		if (p->val < l->val_pivot)
+		if (p->val <= l->val_pivot)
 		{
 			p = p->next;
 			ft_push_b(&l, &lb);
@@ -71,6 +71,7 @@ size_t	ft_a_to_b(t_circlist *l, t_circlist *lb, size_t n, int bol)
 	{
 		while (++i < l->count_rotate)
 			ft_reverse_rotate_a(&l);
+		// ft_sort_others3(l, lb, l->count_rotate, bol);
 		// while (l->count_rotate > 3)
 		// {
 		// 	tree = ft_rbt_create();
@@ -83,40 +84,6 @@ size_t	ft_a_to_b(t_circlist *l, t_circlist *lb, size_t n, int bol)
 	// ft_rbt_freeall(tree);
 	return (top_half_len);
 }
-
-// size_t	ft_b_to_a(t_circlist *l, t_circlist *lb, size_t n, int bol)
-// {
-// 	t_circnode	*p;
-// 	size_t		top_half_len;
-// 	size_t		i;
-
-// 	lb->count_rotate = 0;
-// 	top_half_len = 0;
-// 	p = l->begin;
-// 	i = -1;
-// 	while (++i < n)
-// 	{
-// 		if (p->val >= l->val_pivot)
-// 		{
-// 			p = p->next;
-// 			ft_push_a(&l, &lb);
-// 			top_half_len++;
-// 		}
-// 		else
-// 		{
-// 			p = p->next;
-// 			ft_rotate_b(&l);
-// 			lb->count_rotate++;
-// 		}
-// 	}
-// 	i = -1;
-// 	if (bol)
-// 	{
-// 		while (++i < lb->count_rotate)
-// 			ft_reverse_rotate_a(&l);
-// 	}
-// 	return (top_half_len);
-// }
 
 void	ft_sort_others3(t_circlist *l, t_circlist *lb, size_t n, int bol)
 {
@@ -145,10 +112,13 @@ void	ft_sort_others3(t_circlist *l, t_circlist *lb, size_t n, int bol)
 	while (++i < top_half_len)
 		ft_push_a(&lb, &l);
 
+	ft_sort_three2(l);
+
+
 	ft_sort_others3(l, lb, top_half_len, 1);
 
 	// top_half_len = ft_b_to_a(lb, l, top_half_len, 1);
 	// ft_sort_others3(l, lb, top_half_len, 1);
 
-	ft_sort_three2(l);
+	// ft_sort_three2(l);
 }
